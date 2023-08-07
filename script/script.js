0e39ebc124981f08ee5ca2c3285bf9e9705f40e4
NoaYnov <113116077+NoaYnov@users.noreply.github.com>
2023-08-07 18:37:54 +0200

    adv

--- script/script.js
+++ script/script.js
@@ -2,3 +2,19 @@ function Redirect(link) {
     window.open(link, '_blank');
 
 }
+
+function ExtendToggle(element) {
+    img = element.children[1];
+    if (element.style.height == "800px")
+        element.style.height = "200px",
+        img.style.display = "none";
+    else
+    element.style.height = "800px",
+    img.style.display = "flex";
+
+    
+
+    
+
+}
+
